1c541e85d1e70348386cd5b19db793e8b9bc715b
Kyle McDonald <kyle@kylemcdonald.net>
2012-03-09 20:26:57 -0500

    fsaa what

--- FaceMusic/src/main.cpp
+++ FaceMusic/src/main.cpp
@@ -17,8 +17,9 @@ int main() {
 	
 	int windowWidth = screenWidth + projectorWidth;
 	int windowHeight = MAX(screenHeight, projectorHeight);
-	
+
 	ofAppGlutWindow window;
+	window.setGlutDisplayString("rgba double samples>=8 depth");
 	ofSetupOpenGL(&window, windowWidth, windowHeight, fullscreen ? OF_FULLSCREEN : OF_WINDOW);
 	ofRunApp(new ofApp());
 }
